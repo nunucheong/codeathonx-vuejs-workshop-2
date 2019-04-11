ceebb8a6c2d29cc1c3611471ff9d67cfb7e3e91d
Cheong Chah Wei <nunucheong@gmail.com>
2019-04-12 01:08:46 +0800

    Setup Video Card

--- src/components/SubscriptionFeed.vue
+++ src/components/SubscriptionFeed.vue
@@ -1,7 +1,36 @@
 <template>
   <v-container fill-height>
     <v-layout justify-center align-center>
-      <h1>Hello world</h1>
+      <v-flex xs12 sm6>
+        <v-card>
+          <v-img
+            class="white--text"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">Top 10 Australian beaches</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title>
+            <div>
+              <span class="grey--text">Number 10</span>
+              <br>
+              <span>Whitehaven Beach</span>
+              <br>
+              <span>Whitsunday Island, Whitsunday Islands</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="red">Watch</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
